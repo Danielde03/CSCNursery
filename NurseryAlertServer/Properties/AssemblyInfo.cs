8cb6c36d92f5b9ca418f48d6b3cf3929d03539b4
jdramer <jdramer@users.noreply.github.com>
2014-07-09 09:55:19 -0600

    Tweaks to application properties.
    Use .NET 3.5
    Set property strings.

--- NurseryAlertServer/Properties/AssemblyInfo.cs
+++ NurseryAlertServer/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NurseryAlertServer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Recieve alerts fromt the nursery to display on projector")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Centre Street Church")]
 [assembly: AssemblyProduct("NurseryAlertServer")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyCopyright("Copyright © Holy Spirit 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
