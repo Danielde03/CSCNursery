01b00a1e53cab38f6a79208069a74775b662ae0c
jdramer <jdramer@users.noreply.github.com>
2014-07-10 22:00:38 -0600

    GUI: Add a list view.

--- NurseryAlertServer/MainWindow.Designer.cs
+++ NurseryAlertServer/MainWindow.Designer.cs
@@ -28,13 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listViewEntries = new System.Windows.Forms.ListView();
+            this.colEntryId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colEmergency = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
+            // 
+            // listViewEntries
+            // 
+            this.listViewEntries.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colEntryId,
+            this.colEmergency});
+            this.listViewEntries.GridLines = true;
+            this.listViewEntries.Location = new System.Drawing.Point(12, 42);
+            this.listViewEntries.Name = "listViewEntries";
+            this.listViewEntries.Size = new System.Drawing.Size(494, 225);
+            this.listViewEntries.TabIndex = 0;
+            this.listViewEntries.UseCompatibleStateImageBehavior = false;
+            this.listViewEntries.View = System.Windows.Forms.View.Details;
+            // 
+            // colEntryId
+            // 
+            this.colEntryId.Text = "Entry ID";
+            // 
+            // colEmergency
+            // 
+            this.colEmergency.Text = "E";
+            this.colEmergency.Width = 25;
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(534, 294);
+            this.Controls.Add(this.listViewEntries);
             this.Name = "MainWindow";
             this.Text = "Nursery Alert Server";
             this.Load += new System.EventHandler(this.MainWindow_Load);
@@ -43,6 +69,10 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listViewEntries;
+        private System.Windows.Forms.ColumnHeader colEntryId;
+        private System.Windows.Forms.ColumnHeader colEmergency;
     }
 }
 
