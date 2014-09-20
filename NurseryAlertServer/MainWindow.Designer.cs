44e167bf6ae9fa76879a903576eb9f597c9b6ba3
jdramer <jdramer@users.noreply.github.com>
2014-09-20 15:06:20 -0600

    Add new icon.

--- NurseryAlertServer/MainWindow.Designer.cs
+++ NurseryAlertServer/MainWindow.Designer.cs
@@ -203,6 +203,7 @@
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.listViewEntries);
             this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainWindow";
             this.Text = "CSC Nursery";
