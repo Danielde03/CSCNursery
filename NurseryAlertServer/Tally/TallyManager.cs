5727c6bab5c5dfbf96e17633e0e1e68eac1386bc
Danielde03 <porter.42.de@gmail.com>
2023-11-30 22:17:33 -0700

    Attribution

--- NurseryAlertServer/Tally/TallyManager.cs
+++ NurseryAlertServer/Tally/TallyManager.cs
@@ -16,7 +16,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- *   Author: jdramer
+ *   Author: jdramer, Danielde03
  *
  *   Copyright (c) Holy Spirit
  *
